7253f6131355bd3e9e40a91ede7ce4d41defae37
ShirelleW <shirellewinecoffm@gmail.com>
2022-01-15 10:16:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TriviaInfo from './components/TriviaInfo'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    triviaInfo: {},
+    show: false
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+          fetch("http://jservice.io/api/random")
+          .then(response => response.json())
+          .then(data => this.setState({triviaInfo: data[0]}))
+          .then(() => console.log(this.state.triviaInfo))
+          .catch(error => console.error(error))
+      }
+
+  handleSubmitTwo = e => {
+    e.preventDefault()
+
+          fetch("http://jservice.io/api/random")
+          .then(response => response.json())
+          .then(data => this.setState({triviaInfo: data[0]}))
+          .then(() => console.log(this.state.triviaInfo))
+          .catch(error => console.error(error))
+      }
+    
+
+  render() {
+    const {show} = this.state
+    return (
+      <div id="container">
+        <div>
+        <h1>Welcome to Jeopardy!</h1>
+        <button id="answerbtn" type="submit" onClick={this.handleSubmit} >Get Answer</button><br></br><br></br>
+        
+        <button id="decrease">Decrease</button>
+        <button id="increase">Increase</button>
+        <button id="reset">Reset</button>
+
+        <TriviaInfo triviaInfo ={this.state.triviaInfo} /> 
+        
+        <button id="questionBtn" type="submit" onClick={() => this.setState({show: !show})} >Get Question</button>
+
+        <h4 style={{display: (show ? "block" : "none")}}>Question: {this.state.triviaInfo?.answer}</h4>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
---
+++ src/components/TriviaInfo.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class TriviaInfo extends Component {
+    render(props) {
+        return (
+            <div>
+                <h2>Score: </h2>
+                <h2>Let's Play</h2>
+                <h3>Category: {this.props.triviaInfo.category?.title}</h3>
+                <h3>Points: {this.props.triviaInfo?.value}</h3>
+                <h3>Answer: {this.props.triviaInfo?.question}</h3>
+                {/* <h4 style={{display: (show ? "block" : "none")}}>Question: {this.props.triviaInfo?.answer}</h4> */}
+            </div>
+        );
+    }
+}
+
+export default TriviaInfo;
