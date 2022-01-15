5513f0461daa5e717e9334adc5a1542005a89762
ShirelleW <shirellewinecoffm@gmail.com>
2022-01-15 18:48:52 -0500

    third commit: Decrease/Increase buttons working

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
 
   state = {
     triviaInfo: {},
-    show: false
+    show: false,
+    score: 0
   }
 
   handleSubmit = e => {
@@ -28,7 +29,30 @@ class App extends Component {
           .then(() => console.log(this.state.triviaInfo))
           .catch(error => console.error(error))
       }
+  
+    handleIncrease = () =>{
+      this.setState((prevState) => {
+        return{
+          score: prevState.score+1 
+        }
+      })
+    }
     
+    handleDecrease = () =>{
+      this.setState((prevState) => {
+        return{
+          score: prevState.score-1
+        }
+      })
+    }
+
+    handleReset = () =>{
+      this.setState((prevState) => {
+        return{
+          score: prevState.score = 0 
+        }
+      })
+    }
 
   render() {
     const {show} = this.state
@@ -37,16 +61,19 @@ class App extends Component {
         <div>
         <h1>Welcome to Jeopardy!</h1>
         <button id="answerbtn" type="submit" onClick={this.handleSubmit} >Get Answer</button><br></br><br></br>
-        
-        <button id="decrease">Decrease</button>
-        <button id="increase">Increase</button>
-        <button id="reset">Reset</button>
 
         <TriviaInfo triviaInfo ={this.state.triviaInfo} /> 
         
         <button id="questionBtn" type="submit" onClick={() => this.setState({show: !show})} >Get Question</button>
 
         <h4 style={{display: (show ? "block" : "none")}}>Question: {this.state.triviaInfo?.answer}</h4>
+
+        <div>
+          <h2>Score: {this.state.score}</h2>
+          <button id="decrease" onClick={this.handleDecrease.bind(this)}>Decrease</button>
+          <button id="increase" onClick={this.handleIncrease.bind(this)}>Increase</button>
+          <button id="reset" onClick={this.handleReset.bind(this)}>Reset</button>
+        </div>
         </div>
       </div>
     );
